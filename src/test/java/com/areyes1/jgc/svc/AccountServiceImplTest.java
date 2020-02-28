392c838c3f4d8fca9bbc078fec5b28e6c96a3f89
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2020-02-28 15:32:27 +0800

    [add] Create a new test code

---
+++ src/test/java/com/areyes1/jgc/svc/AccountServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.areyes1.jgc.svc;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AccountServiceImplTest {
+
+    @Test
+    public void testCreateNewAccount() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    public void testRemoveAccount() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    public void testListAllTransactions() {
+        fail("まだ実装されていません");
+    }
+
+}
